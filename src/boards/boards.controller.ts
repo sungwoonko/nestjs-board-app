4b11b39ba72cccb9372a88ff4c8f57b876e06d03
sungwoonko <ko_sw@naver.com>
2025-01-21 15:13:54 +0900

    feat : 게시글 작성 기능 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.entity';
+import { title } from 'process';
 
-@Controller('boards')
+@Controller('api/boards')
 export class BoardsController {
     // 생성자 주입
     constructor(private boardsService :BoardsService){}
@@ -11,6 +12,16 @@ export class BoardsController {
     @Get('/')
     getAllBoards(): Board[] {
         return this.boardsService.getAllBoards();
+    }
+
+    // 게시글 작성 기능
+    @Post('/')
+    createBoards(
+        @Body('author') author: string,
+        @Body('title') title: string,
+        @Body('contents') contents: string,
+    ) {
+        return this.boardsService.createBoard(author, title, contents);
     }
 
 }
