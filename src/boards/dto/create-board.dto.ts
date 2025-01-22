015631d30fa7b174204f886f09449249295fb47f
sungwoonko <ko_sw@naver.com>
2025-01-22 15:55:24 +0900

    refactor : 정규표현식(RegEx) 사용방법 @Matches

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class createBoardDto {
     @IsNotEmpty()
@@ -12,4 +12,8 @@ export class createBoardDto {
     @IsNotEmpty()
     @IsString()
     contents: string;
+
+
+    // @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, { message: 'Password too weak', }) // 대문자, 소문자, 숫자, 특수문자 포함
+    // password: string;
 }
