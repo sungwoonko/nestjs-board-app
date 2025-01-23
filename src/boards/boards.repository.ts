d90d12907987b11dae9ffe1e04c2536fa5c953fa
sungwoonko <ko_sw@naver.com>
2025-01-23 15:41:50 +0900

    refactor : Repository 계층 생성 및 서비스 코드 분리

---
+++ src/boards/boards.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { createPool,Pool} from "mysql2/promise";
+import { databaseConfig } from "../configs/database.config";
+import { error } from "console";
+import { Board } from "./boards.entity";
+
+@Injectable()
+export class BoardsRepository{
+    private connectPool: Pool
+    
+    
+    constructor() {
+        this.connectPool = createPool(databaseConfig);
+        this.connectPool.getConnection()
+            .then(() => console.log('DB Connected'))
+            .catch(err => console.error('DB connection faild',err));
+    }
+
+    // 게시글 조회관련 데이터 엑세스
+    async fingAll(): Promise<Board[]> {
+        const selectQuery = `SELECT * FROM board`;
+        try{
+           const[result] = await this.connectPool.query(selectQuery)
+           return result as Board[]
+        }catch(err) {
+            throw new InternalServerErrorException('Database query failed',err);            
+        }
+    }
+
+}
