b2ff574ff3d2aba33dbfb51ea421c55fa608391f
sungwoonko <ko_sw@naver.com>
2025-02-04 11:12:08 +0900

    refactor : ValidationPipe 전역 설정으로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { GlobalModule } from './global.module';
 
 
 
 @Module({
   imports: [
+    GlobalModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     BoardsModule,
     AuthModule,
