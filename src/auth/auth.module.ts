6fee45716b6bffd8b92af8b00ce7873d66cbde7e
sungwoonko <ko_sw@naver.com>
2025-02-04 13:59:06 +0900

    chore : JWT 모듈 및 연결 의존성 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports:[
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRATION,
+      }
+    }),
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
