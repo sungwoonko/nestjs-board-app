08b807620902b3cb4a3455755b1cc40a642d88b1
sungwoonko <ko_sw@naver.com>
2025-02-05 10:15:18 +0900

    feat : 커스텀 데코레이터 생성 부분

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,8 +3,10 @@ import { AuthService } from './auth.service';
 import { UserResponseDto } from './dto/user-response.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user-decorator';
+import { User } from './users.entity';
 
 @Controller('api/auth')
 export class AuthController {
@@ -34,9 +36,10 @@ export class AuthController {
 
       @Post('/test')
       @UseGuards(AuthGuard('jwt')) // @UseGuards는 해당 인증 가드가 적용되는, AuthGuard는 인증가드가 어떤 전략을 사용할지 결정
-      testForAuth(@Req() req: Request){
-        console.log(req.user); // 인증된 사용자의 정보 출력
-        return { message : 'Authenticatied User', user: req.user };
+      testForAuth(@GetUser() loggineduser: User){
+        console.log(loggineduser); // 인증된 사용자의 정보 출력
+        console.log(loggineduser.email); // 인증된 사용자의 특정 필드 접근
+        return { message : 'Authenticatied User', user: loggineduser };
       }
 }
  
